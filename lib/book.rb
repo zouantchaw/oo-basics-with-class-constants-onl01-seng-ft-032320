22500fe2e264d3716bde2d155837c2a5142d661d
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-30 18:03:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
+  attr_accessor :author, :page_count  
+  attr_reader :title, :genre  
  
   GENRES = []
  
@@ -12,7 +12,6 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
  
-  # create the writer for genre and add the logic for the class constant
   def genre=(genre)
     @genre = genre
     GENRES << genre 
